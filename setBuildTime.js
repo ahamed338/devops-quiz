77ed9c5858141d001eab0584b842951557a91c79
Ahamed <ahamed338@hotmail.com>
2025-09-30 20:19:12 +0530

    removed unnecessary files

--- setBuildTime.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const buildTime = new Date().toISOString();
-const envFile = path.join(__dirname, ".env");
-
-fs.writeFileSync(envFile, `REACT_APP_BUILD_TIME=${buildTime}\n`);
-console.log("Updated build time:", buildTime);
